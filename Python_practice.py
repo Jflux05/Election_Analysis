e63e6db5dfaf50beabb3cca7cb13a41ab27bad6d
JED FLUXMAN <jedfluxman@JEDs-MacBook-Pro.local>
2021-11-08 21:09:52 -0800

    initial commit

--- Python_practice.py
+++ Python_practice.py
@@ -1,2 +1,3 @@
-print("Hello World")
+voting_data = [``{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
 
+    
